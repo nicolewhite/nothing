7d4fbaf0a0032ad676a60e4ef1b9109b97a9deab
Nicole White <nicole.m.white@outlook.com>
2015-08-11 18:39:28 -0700

    commonality

--- blog/models.py
+++ blog/models.py
@@ -82,6 +82,27 @@ class User:
 
         return graph.cypher.execute(query, username=self.username, n=n)
 
+    def commonality_of_user(self, user):
+        query1 = """
+        MATCH (user1:User)-[:PUBLISHED]->(post:Post)<-[:LIKES]-(user2:User)
+        WHERE user1.username = {username1} AND user2.username = {username2}
+        RETURN COUNT(post) AS likes
+        """
+
+        likes = graph.cypher.execute_one(query1, username1=self.username, username2=user.username)
+        likes = 0 if not likes else likes
+
+        query2 = """
+        MATCH (user1:User)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag:Tag),
+              (user2:User)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag)
+        WHERE user1.username = {username1} AND user2.username = {username2}
+        RETURN COLLECT(DISTINCT tag.name) AS tags
+        """
+
+        tags = graph.cypher.execute(query2, username1=self.username, username2=user.username)[0]["tags"]
+
+        return {"likes": likes, "tags": tags}
+
 
 def todays_recent_posts(n):
     query = """
